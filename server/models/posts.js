b111022ea673859bb3ff1505325a9dce54b53081
chiboycalix <igwechinonso77@gmail.com>
2019-09-27 07:07:54 +0100

    setup routing

---
+++ server/models/posts.js
@@ -0,0 +1,16 @@
+const posts = [
+  {
+    title: 'this is the first title',
+    body: 'this is the first body',
+  },
+  {
+    title: 'this is the second title',
+    body: 'this is the second body',
+  },
+  {
+    title: 'this is the third title',
+    body: 'this is the third body',
+  },
+];
+
+export default posts;
